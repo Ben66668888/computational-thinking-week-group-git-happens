0a6d74863ffe480b154476fce2cb69a9fe9fcaac
yozmbread <yozmbread@gmail.com>
2026-08-31 11:54:58 +0200

    Update git_exercise.py

--- challenge_day3/git_exercise.py
+++ challenge_day3/git_exercise.py
@@ -2,6 +2,7 @@ from ben import get_name as get_ben_name
 from oumaima import get_name as get_oumaima_name
 from yuv import get_name as get_yuv_name
 from SihunYou import get_name as get_SihunYou_name
+from yus import get_name as get_yus_name
 
 
 def team_members():
@@ -10,5 +11,6 @@ def team_members():
     print(get_oumaima_name())
     print(get_yuv_name())
     print(get_SihunYou_name())
+    print(get_yus_name())
 
 team_members()
