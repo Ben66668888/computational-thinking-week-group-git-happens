4ac197c15772c051b3185576ea4b29ac3ff8c659
yozmbread <yozmbread@gmail.com>
2026-08-31 11:39:47 +0200

    Update git_exercise.py

--- challenge_day3/git_exercise.py
+++ challenge_day3/git_exercise.py
@@ -1,13 +1,14 @@
 from ben import get_name as get_ben_name
 from oumaima import get_name as get_oumaima_name
 from yuv import get_name as get_yuv_name
-
+from SihunYou import get_name as get_SihunYou_name
 
 def team_members():
     print("This is Team Git Happens. We are:")
     print(get_ben_name())
     print(get_oumaima_name())
     print(get_yuv_name())
+    print(get_SihunYou_name())
 
 
 team_members()
