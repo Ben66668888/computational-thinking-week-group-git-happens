a99f8b71947db94f8073a0a602c0a5d660e88b45
Oumaima <oumaima.aouled.ben.hssaien@student.uva.nl>
2026-08-31 11:46:35 +0200

    Clean up merge conflict markers

--- challenge_day3/git_exercise.py
+++ challenge_day3/git_exercise.py
@@ -2,10 +2,7 @@ from ben import get_name as get_ben_name
 from oumaima import get_name as get_oumaima_name
 from yuv import get_name as get_yuv_name
 from SihunYou import get_name as get_SihunYou_name
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 
 def team_members():
     print("This is Team Git Happens. We are:")
@@ -13,9 +10,5 @@ def team_members():
     print(get_oumaima_name())
     print(get_yuv_name())
     print(get_SihunYou_name())
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 team_members()
